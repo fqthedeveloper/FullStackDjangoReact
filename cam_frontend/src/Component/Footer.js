fbce0aef5a259fd14626f9446d1dc2b4376a52fa
Faizan Qureshi <74370508+fqthedeveloper@users.noreply.github.com>
2022-10-28 04:39:20 -0700

    this is updated footer

--- cam_frontend/src/Component/Footer.js
+++ cam_frontend/src/Component/Footer.js
@@ -34,6 +34,15 @@ function Footer() {
                     <li className='nav-item'><Link to="/" className='nav-link px-2 text-white '>Home</Link></li>
                     <li className='nav-item'><Link to="/contact" className='nav-link px-2 text-white '>Contect US</Link></li>
                 </ul>
+                <ul>
+                <Link to='/'><i  class="bi bi-twitter fa-2x"></i></Link>
+                     &nbsp; &nbsp; &nbsp; 
+                     <Link to='/'><i class="bi bi-instagram fa-2x"></i></Link>
+                     &nbsp; &nbsp; &nbsp; 
+                     <Link to='/'><i class="bi bi-whatsapp fa-2x"></i></Link>
+                     &nbsp; &nbsp; &nbsp; 
+                     <Link to='/'><i class="bi bi-facebook fa-2x"></i></Link>
+                </ul>
                 <b> © 2022 Copyright: <Link className='text-white' to='/'>Cammra Web </Link></b>
             </footer>
         </div>
